2d14ad579ae47831cc49ad2ba348f4b7d69bd33f
Malachai <xogmldntmd@naver.com>
2022-03-10 20:36:31 +0900

    220310 - 6강까지

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,32 +1,99 @@
 <template>
+
+  <div class='black-bg' v-if='ui_state.IsModalOn == true'>
+    <div class='white-bg'>
+      <h4>{{ ui_state.current_product_title }}</h4>
+      <p>{{ ui_state.current_product_detail }}</p>
+      <button @click='btn_onclick_modal_close'>닫기</button>
+    </div>
+  </div>
+
+
   <div class='menu'>
     <a v-for="elem in menus" :key="elem">{{ elem }}</a>
   </div>
 
-  <div v-for='(elem, i) in 3' :key="i">
-    <h4 class='cls'>{{ products[i] }} 원룸</h4>
-    <p :style='price_color'>{{ prices[i] }} 만원</p>
+  <div class='product' v-for='(elem, i) in products' :key="i">
+    <img class='products-img' :src='products[i].image'>
+    <h4 class='cls'>{{ products[i].title }}</h4>
+
+    <p :style='price_color'>{{ products[i].price }} 만원</p>
+    <button @click='btn_onclick_modal_product_detail(i)'>상세보기</button>
+    <button @click='btn_onclick_report(i)'>Report</button> <span>신고수: {{ reports[i] }}</span>
   </div>
+
+  
 
 </template>
 
 <script>
+import {products} from './data/posts.js';
+
 export default {
   name: 'App',
+
   data(){
     return {
+      ui_state : {
+        IsModalOn : false,
+        IsMenuOn : false,
+        current_product_title : '',
+        current_product_detail : ''
+      },
       menus : ['홈', '검색', '마이페이지'],
-      products : ['역삼', '천호', '마포'],
-      prices : [50, 70, 90],
+      products : products,
+      reports : [0, 0, 0],
       price_color : 'color: darkgray'
     }
   },
+
+  methods : {
+    btn_onclick_report(i){
+      this.reports[i] += 1;
+    },
+    btn_onclick_modal_product_detail(i){
+      this.ui_state.current_product_title = this.products[i].title;
+      this.ui_state.current_product_detail = this.products[i].content;
+      this.ui_state.IsModalOn = true;
+    },
+    btn_onclick_modal_close(){
+      this.ui_state.IsModalOn = false;
+    }
+  },
+
   components: {
+    
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0px;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  display: flex;
+  justify-content: center;
+  width: 100%; height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg {
+  display: inline-block;
+  width: 50%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -34,6 +101,7 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
+
 
 .menu {
   background: darkslateblue;
@@ -44,6 +112,14 @@ export default {
 .menu a {
   color: white;
   padding: 10px;
+}
+
+.product {
+  margin-top: 30px;
+}
+
+.products-img {
+  max-width: 30%;
 }
 
 </style>
